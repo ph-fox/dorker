ee58f2762929f33fb17de9c99f84c15adf93b5be
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-08-07 18:40:02 +0800

    Update new_dorker.py

--- new_dorker.py
+++ new_dorker.py
@@ -1,4 +1,4 @@
-import os, argparse
+import os, argparse, time
 parser = argparse.ArgumentParser()
 parser.add_argument('-d','--dork',help="Enter your dork")
 parser.add_argument('-a','--amount',help="Enter amount of site to printout")
@@ -6,27 +6,31 @@ arg = parser.parse_args()
 
 class Dorker:
 	def __init__(self, dork, amount):
-		self.dork = dork
-		self.amount = amount
+	 self.dork = dork
+	 self.amount = amount
 
 	def check_requirements(self):
 		try:
 		 from googlesearch import search
-		 if(self.dork and self.amount is None):
+		 if(self.dork is None and self.amount is None):
 		  print(f'Usage: python3 {os.path.basename(__file__)} -d <dork> -a <amount>')
+		  exit()
 		 else:
-		  dork.searcher()
+		  dork.searcher(search)
 		except ModuleNotFoundError:
 		 print("Installing Required Module!..")
 		 os.system("pip3 install googlesearch-python")
 		except Exception as err:
 		 print("Error: {}".format(err))
-	def searcher(self):
+	def searcher(self,search):
 		count=0
 		print("="*14+"-Dorking-"+"="*14)
+		print()
 		for site in search(self.dork, num_results=int(self.amount)):
 		 count+=1
 		 print(f'({count}):  {site}')
+		 time.sleep(.1)
+		print()
 		print("="*14+"Done"+"="*14)
 
 if __name__=="__main__":
@@ -37,6 +41,7 @@ if __name__=="__main__":
 888    | 8888   | 888    888Y88b   8888__888 888    
 888   /  Y888   ' 888    888 Y88b  Y888    , 888    
 888_-~    "88_-~  888    888  Y88b  "88___/  888    
+
 DORKER BY: Anikin Luke
  """)
  dork = Dorker(arg.dork, arg.amount)
